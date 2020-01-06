48a92e398965d1fcf1769910500ec7a91c94fae5
MatanMaimon <mataname@gmail.com>
2020-01-06 13:53:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,27 +4,27 @@ import { exec } from 'child_process';
 import express from 'express';
 import bodyParser from 'body-parser';
 
-
 const app = express();
 
-
 const GITHUB_TO_DIR = {
-  'MatanMaimon/ResToRent__server': [{
-    destDir: `~/gitRepos/ResToRent`,
-    isServerSide: true,
-    appName: 'app'
-  }],
-  'MatanMaimon/ResToRent__client': [{
-    destDir:`/var/www/ResToRent`,
-    isClientSide: true
-  }],
+  'MatanMaimon/ResToRent__server': [
+    {
+      destDir: `~/gitRepos/ResToRent`,
+      isServerSide: true,
+      appName: 'app',
+    },
+  ],
+  'MatanMaimon/ResToRent__client': [
+    {
+      destDir: `/var/www/ResToRent`,
+      isClientSide: true,
+    },
+  ],
 };
-
 
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-
   // output to the end
   let output = `Starting...\n`;
 
@@ -36,51 +36,49 @@ app.post('/', (req, res) => {
     .update(stringifyBody)
     .digest('hex')}`;
 
-
   const isAllowed = req.headers['x-hub-signature'] === signature;
   const isMaster = req.body?.ref === 'refs/heads/master';
   const directory = GITHUB_TO_DIR[req.body?.repository?.full_name];
-      
-  if (isAllowed && isMaster && directory && directory.length) {
 
+  if (isAllowed && isMaster && directory && directory.length) {
     try {
       // the webhook.sh dir
       const webHookPath = __dirname;
 
       // execute for each `directory` item
       directory.forEach(entry => {
-
-        
         // first, pull
         output += `pulling "master" branch to ${entry.destDir}...\n`;
         exec(`cd ${entry.destDir} && git pull --rebase origin master`);
 
         // check if need to `npm install` (if package.json was modified)
-        if (req.body?.commits?.filter(commit => commit.modified?.indexOf('package.json') >= 0).length > 0) {
-          output += `${req.body?.commits[0].modified.indexOf('package.json')} npm install is need to be done ("package.json") was modified\n`;
+        if (
+          req.body?.commits?.filter(
+            commit => commit.modified?.indexOf('package.json') >= 0,
+          ).length > 0
+        ) {
+          output += `npm install is need to be done ("package.json") was modified\n`;
           exec(`cd ${entry.destDir} && npm install`);
         }
 
         // if the repo is server side (nodejs), restart the app
         if (entry.isServerSide) {
           output += `this repo is server side, restart pm2 for the app "${entry.appName}"\n`;
-          exec(`pm2 stop ${entry.appName}`);
-          exec(`pm2 restart ${entry.appName}`);
+          // exec(`cd ${entry.destDir} && pm2 stop ecosystem.config.js`);
+          exec(`cd ${entry.destDir} && pm2 restart ecosystem.config.js`);
         }
 
-        
         //exec(`cd ${entry} && bash ${webHookPath}/../webhook.sh`)
       });
 
       output += `DONE!`;
       console.log('success');
     } catch (error) {
-      
       output += `ERROR!`;
       console.log(error);
     }
   }
   res.send(output);
 });
-      
+
 app.listen(8073, () => console.log(`autoDeploy webhook is running!`));
