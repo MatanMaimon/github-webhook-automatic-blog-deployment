bda5358feab0c6685a7d96cb5c8801264a0ca684
MatanMaimon <mataname@gmail.com>
2020-01-06 12:55:10 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,6 @@ module.exports = {
   apps : [{
     name: 'autoDeploy-webhook',
     script: 'npm start',
-
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-decl$
-    // args: 'one two',
-    // instances: 1,
     autorestart: true,
     watch: true,
     max_memory_restart: '256M',
