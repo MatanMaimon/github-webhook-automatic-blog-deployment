8f161415b394ce1be1bab0d989435358d09efa4d
MatanMaimon <mataname@gmail.com>
2020-01-06 13:15:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,15 @@ const app = express();
 
 
 const GITHUB_TO_DIR = {
-  'MatanMaimon/ResToRent': [
-    `/var/www/ResToRent`,
-  ],
+  'MatanMaimon/ResToRent__server': [{
+    destDir: `~/gitRepos/ResToRent`,
+    isServerSide: true,
+    appName: 'app'
+  }],
+  'MatanMaimon/ResToRent__client': [{
+    destDir:`/var/www/ResToRent`,
+    isClientSide: true
+  }],
 };
 
 
@@ -33,19 +39,47 @@ app.post('/', (req, res) => {
   const directory = GITHUB_TO_DIR[req.body?.repository?.full_name];
       
   if (isAllowed && isMaster && directory && directory.length) {
+    // output to the end
+    let output = `Starting...\n`;
+
     try {
-      
       // the webhook.sh dir
       const webHookPath = __dirname;
 
       // execute for each `directory` item
-      directory.forEach(entry => exec(`cd ${entry} && bash ${webHookPath}/../webhook.sh`));
+      directory.forEach(entry => {
+
+        
+        // first, pull
+        output = `pulling "master" branch to ${entry.destDir}...\n`;
+        exec(`cd ${entry.destDir} && git pull --rebase origin master`);
+
+        // check if need to `npm install` (if package.json was modified)
+        if (req.body?.commits?.filter(commit => commit.modified?.indexOf('package.json')).length > 0) {
+          output = `npm install is need to be done ("package.json") was modified\n`;
+          exec(`cd ${entry.destDir} && npm install`);
+        }
+
+        // if the repo is server side (nodejs), restart the app
+        if (entry.isServerSide) {
+          output = `this repo is server side, restart pm2 for the app "${entry.appName}"\n`;
+          exec(`pm2 stop ${entry.appName}`);
+          exec(`pm2 restart ${entry.appName}`);
+        }
+
+        
+        //exec(`cd ${entry} && bash ${webHookPath}/../webhook.sh`)
+      });
+
+      output = `DONE!`;
       console.log('success');
     } catch (error) {
+      
+      output = `ERROR!`;
       console.log(error);
     }
   }
-  res.send(200);
+  res.send(output);
 });
       
 app.listen(8073, () => console.log(`autoDeploy webhook is running!`));
